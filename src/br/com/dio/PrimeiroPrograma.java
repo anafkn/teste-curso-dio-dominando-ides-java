655c047ffb0c64fa17eee73c0af66d761846911e
User <anafkn@hotmail.com>
2023-04-10 22:02:12 -0300

    First Commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato(); 
+		Livros livros = new Livros();
+	 
+		System.out.printIn(gato);
+		System.out.printIn(gato);
+	}
+	
+}	
+	class Livros {
+	private String nome;
+	private String npag;
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author User 3869
+ *
+ */
+module primeiroprogramajava {
+}
